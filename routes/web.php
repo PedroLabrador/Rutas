7ee3b91699c55d66dd81801ccc5f491c622040ba
PedroLabrador <pedro.labradorp@unet.edu.ve>
2018-03-14 16:43:40 -0430

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function() {
-  return view('welcome');
-});
+Route::get('/', 'HomeController@home');
+
+Route::get('/{ruta}', 'HomeController@listar');
+
+Route::post('/registrar', 'HomeController@registrar');
