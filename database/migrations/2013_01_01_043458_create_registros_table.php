98073317103c4b4d65e9f746aef523af732a1ac8
PedroLabrador <pedro.labradorp@unet.edu.ve>
2018-03-14 16:43:35 -0430

    Migraciones

---
+++ database/migrations/2013_01_01_043458_create_registros_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('municipio_id')->unsigned();
+            $table->string('cedula', 16);
+            $table->string('nombre', 64);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registros');
+    }
+}
