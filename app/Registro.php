d5b0e4db7075ed9e703c3e60cf8c2d88cd3d23e5
PedroLabrador <pedro.labradorp@unet.edu.ve>
2018-03-15 08:28:45 -0430

    Nuevo campo en el modelo

--- app/Registro.php
+++ app/Registro.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Registro extends Model
 {
     protected $table = 'registros';
-    protected $fillable = ['cedula', 'nombre', 'hora', 'municipio_id'];
+    protected $fillable = ['cedula', 'nombre', 'hora', 'municipio_id', 'intentos'];
 }
