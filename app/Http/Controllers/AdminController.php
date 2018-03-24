9ebe9652afc2c33d9bf50674631215a3a43a8d13
PedroLabrador <pedrolabrador3@outlook.com>
2018-03-24 17:22:11 -0400

    Mostrar vista para crear horario

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Horario;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index() {
     	return view('admin.index');
+    }
+
+    public function mostrar() {
+        return view('admin.show');
     }
 }
