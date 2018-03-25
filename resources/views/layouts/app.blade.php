325daa14d762674aeeca21184aa684831b981120
PedroLabrador <pedrolabrador3@outlook.com>
2018-03-25 11:50:00 -0400

    Clase de css agregada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Rutas') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -30,6 +30,12 @@
         .float-right {
             float: right;
             padding: .5em
+        }
+        .badge-default {
+            background: #333;
+            color: white;   
+            padding: 5px 10px;
+            border-radius: 25%;
         }
     </style>
 </head>
