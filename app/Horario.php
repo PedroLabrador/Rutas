3e077cb87fc85dbb78c0294a18b705ad3cf00da0
PedroLabrador <pedrolabrador3@outlook.com>
2018-04-24 22:34:41 -0430

    Campo nuevo en la tabla horarios

--- app/Horario.php
+++ app/Horario.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Horario extends Model
 {
-	protected $fillable = ['municipio_id', 'hora'];
+	protected $fillable = ['municipio_id', 'hora', 'check'];
 
 	public $timestamps = false;
 
