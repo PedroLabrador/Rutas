44e6523ec4a1b75b5b584de4a333326199bdf4cf
PedroLabrador <pedrolabrador3@outlook.com>
2018-04-24 22:35:15 -0430

    Boton eliminar desabilitado por precaución

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Horario;
+use App\Municipio;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -20,10 +21,12 @@ class AdminController extends Controller
             'hour' => 'required',
         ], [
             'hour.required' => 'La hora es obligatoria.'
-        ]);	
+        ]);
 
     	$data['municipio_id'] = \Auth::user()->municipio->id;
     	$data['hora'] =  date('h:i A', strtotime($request->input('hour')));
+        $data['check'] = true;
+
     	Horario::create($data);
         return redirect('/crear')->with('flash_message', 'Hora creada!');
     }
@@ -31,20 +34,25 @@ class AdminController extends Controller
     public function mostrar($id) {
         $horario = Horario::where('id', $id)->first();
         $hora = date('H:i', strtotime($horario->hora));
-        return view('admin.edit', ['hora' => $hora, 'id' => $horario->id]);
+        return view('admin.edit', ['hora' => $hora, 'id' => $horario->id, 'check' => $horario->check]);
     }
 
     public function editar(Request $request) {
         $id = $request->input('id');
         $data['hora'] = date('h:i A', strtotime($request->input('hour')));
+        $data['check'] = ($request->input('check') == 'on') ? true : false;
+
         $horario = Horario::where('id', $id);
         $horario->update($data);
         return redirect('/admin')->with('flash_message', 'Hora editada!');
     }
 
-    public function borrar($id) {
-        $horario = Horario::where('id', $id)->first();
-        Horario::destroy($id);
-        return redirect('/admin')->with('delete', 'Horario '. $horario->hora .' borrado!');
+    public function anotar(Municipio $municipio) {
+        $horarios = Horario::where('municipio_id', $municipio->id)->
+                             where('check', true)->
+                             get();
+        return view('admin.anotar', [
+            'horarios' => $horarios
+        ]);
     }
 }
