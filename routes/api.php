556bf947989ff88be4ecd392c2c14a4eefb0364b
PedroLabrador <pedrolabrador3@outlook.com>
2018-04-24 23:01:36 -0430

    Ruta para migrar

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*Route::get('/migrar', function() {
+   return Artisan::call('migrate:refresh', array('--force' => true));
+});*/
